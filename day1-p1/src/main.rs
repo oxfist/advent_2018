c5b3aa071383489078a0380672368ebfc1b36aeb
Andrés Quilodrán <andres.caro.q@gmail.com>
2018-12-10 01:42:48 -0300

    DAY-1 Add base program for printing input file lines

--- day1-p1/src/main.rs
+++ day1-p1/src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let content = fs::read_to_string(filename);
+
+    for frequency in content {
+        println!("{}", frequency);
+    }
 }
