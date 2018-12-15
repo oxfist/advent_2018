5fad1272f3713025a821ab4497dddfb7e6bcb798
Andrés Quilodrán <andres.c@thoughtworks.com>
2018-12-15 16:18:20 -0300

    DAY-1 Add extra function and passing of a hash map to d1p2

--- day1-p2/src/main.rs
+++ day1-p2/src/main.rs
@@ -1,21 +1,29 @@
 use std::env;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-    let file = File::open(filename).expect("Existing file");
-    let mut current_frequency: i32 = 0;
 
-    read_frequencies(&mut current_frequency, file);
+    chronal_calibration(filename.to_string());
 }
 
-fn read_frequencies(current_frequency: &mut i32, file: File) {
+fn chronal_calibration(filename: String) {
+    let file = File::open(filename).expect("Existing file");
+    let mut current_frequency: i32 = 0;
+    let frequencies = HashMap::new();
+
+    read_frequencies(&mut current_frequency, frequencies, file);
+
+    println!("{}", current_frequency);
+}
+
+fn read_frequencies(current_frequency: &mut i32,
+                    frequencies: HashMap<&str, i32>, file: File) {
     for line in BufReader::new(file).lines() {
         let new_frequency: i32 = line.unwrap().parse().expect("Valid integer");
         *current_frequency += new_frequency;
     }
-
-    println!("{}", current_frequency);
 }
