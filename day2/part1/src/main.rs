06fa2019872f311088bea34d853a0d55b4d6a7ba
Andrés Quilodrán <andres.caro.q@gmail.com>
2018-12-16 19:11:51 -0300

    DAY-2 Add code for reading input file

--- day2/part1/src/main.rs
+++ day2/part1/src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let filename = env::args().nth(1).expect("A valid string");
+
+    inventory_management_system(filename.to_string());
+}
+
+fn inventory_management_system(filename: String) {
+    let file = File::open(filename).expect("Existing file");
+
+    for line in BufReader::new(file).lines() {
+        println!("{}", line.expect("Valid string"));
+    }
 }
